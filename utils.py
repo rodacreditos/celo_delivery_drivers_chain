6811394c26f1adc1cd576fe4160f5a90935b5cec
james-payan <james.payan@correounivalle.edu.co>
2023-12-22 17:54:52 -0500

    added utility module

---
+++ utils.py
@@ -0,0 +1,17 @@
+import csv
+
+
+def dicts_to_csv(dict_list, filename):
+    """
+    Converts a list of dictionaries with the same keys into a CSV file.
+
+    :param dict_list: List of dictionaries
+    :param filename: Filename for the output CSV file
+    """
+    if not dict_list:
+        raise ValueError("The list of dictionaries is empty.")
+
+    with open(filename, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=dict_list[0].keys())
+        writer.writeheader()
+        writer.writerows(dict_list)

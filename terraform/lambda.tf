9a8fb0fbbe5e99498d0ff0a856dd27c7767c5724
james-payan <james.payan@correounivalle.edu.co>
2023-12-25 20:45:53 -0500

    added terraform code for deploying the lambda functions

---
+++ terraform/lambda.tf
@@ -0,0 +1,58 @@
+resource "aws_lambda_function" "tribu_extraction" {
+  function_name = "extract_tribu_data"
+
+  package_type = "Image"
+  image_uri    = "062988117074.dkr.ecr.us-east-2.amazonaws.com/rodaapp:tribu_extraction"
+
+  role    = aws_iam_role.lambda_exec_role.arn
+
+  
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+      },
+    ],
+  })
+}
+
+resource "aws_iam_policy" "lambda_s3_access" {
+  name        = "LambdaS3AccessPolicy"
+  description = "Allow Lambda function to access a specific rodaapp-rappidriverchain S3 bucket"
+
+  policy = jsonencode({
+    Version = "2023-12-25",
+    Statement = [
+      {
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+        ],
+        Effect = "Allow",
+        Resource = [
+          "arn:aws:s3:::rodaapp-rappidriverchain/*",
+        ],
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3_attach" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.lambda_s3_access.arn
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}

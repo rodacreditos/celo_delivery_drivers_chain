7d43d284a77d412626acab4c15171bab8ef43ef7
james-payan <james.payan@correounivalle.edu.co>
2023-12-26 13:13:45 -0500

    amended because eventbridge is not sending any nested dict for the input payload that we configure

--- tribu_datapipeline/lambda_extract_tribu_data.py
+++ tribu_datapipeline/lambda_extract_tribu_data.py
@@ -62,11 +62,11 @@ def get_tribu_data(token, date):
 def handler(event, context):
     logger.setLevel(logging.INFO)
     logger.info(f"STARTING: Tribu data extraction task. Parameters: \n{event}")
-    tribu_token = login(event["detail"]["dataset_type"])
+    tribu_token = login(event["dataset_type"])
     processing_date = event.get("processing_date")
     processing_date = validate_date(processing_date) if processing_date else yesterday()
     output_path = os.path.join(RODAAPP_BUCKET_PREFIX, "tribu_data", f"date={format_dashed_date(processing_date)}", 
-                               f"tribu_{event['detail']['dataset_type']}_routes.csv")
+                               f"tribu_{event['dataset_type']}_routes.csv")
 
     tribu_data = get_tribu_data(tribu_token, processing_date)
     
@@ -87,10 +87,7 @@ if __name__ == "__main__":
         args = parser.parse_args()
         setup_local_logger() # when it does not have env vars from aws, it means that this script is running locally 
         if args.date:
-            handler(dict(
-                        detail=dict(
-                            processing_date=format_dashed_date(args.date),
-                            dataset_type=args.dataset_type)
-                        ), "dockerlocal")
+            handler(dict(processing_date=format_dashed_date(args.date),
+                            dataset_type=args.dataset_type), "dockerlocal")
         else:
-            handler(dict(detail=dict(dataset_type=args.dataset_type)), "dockerlocal")
+            handler(dict(dataset_type=args.dataset_type), "dockerlocal")

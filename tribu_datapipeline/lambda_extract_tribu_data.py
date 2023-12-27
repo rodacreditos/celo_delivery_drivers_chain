8663db3fb5e38fca43382d6ca144698060b7cee4
james-payan <james.payan@correounivalle.edu.co>
2023-12-27 12:42:42 -0500

    added docstring for the lambda extraction script

--- tribu_datapipeline/lambda_extract_tribu_data.py
+++ tribu_datapipeline/lambda_extract_tribu_data.py
@@ -1,6 +1,42 @@
 """
-This script connects to Tribu API and gets the GPS data of the rappi driver bicycles, from especified date.
-Given the dataset type, this script will search for the credentials of tribu api in order to download the right dataset
+This script connects to the Tribu API to retrieve GPS data of Rappi drivers. It handles two types of datasets: 
+'roda', primarily consisting of motorbike data, and 'guajira', primarily consisting of bicycle data. The script 
+fetches appropriate Tribu API credentials based on the specified dataset type and downloads the relevant dataset.
+Designed for deployment in a Docker container, it's suitable for execution in an AWS Lambda function and supports
+local testing.
+
+The script can be executed in various environments:
+1. As an AWS Lambda function - It is designed to run within AWS Lambda, fetching parameters from the event object.
+2. In a Docker container - Suitable for local testing or deployment.
+3. Directly via CLI - For local execution and testing.
+
+The script supports command-line arguments for easy local testing and debugging. It leverages functionality 
+from an accompanying 'utils.py' module for tasks like data processing and AWS S3 interactions.
+
+Environment Variables:
+- AWS_LAMBDA_RUNTIME_API: Used to determine if the script is running in an AWS Lambda environment.
+
+Usage:
+- AWS Lambda: Deploy the script as a Lambda function. The handler function will be invoked with event and context parameters.
+- Docker Container/CLI: Run the script with optional command-line arguments to specify the dataset type and processing date.
+
+Command-Line Arguments:
+- --date (-d): Optional. Specify the date for data retrieval in 'YYYY-MM-DD' format. If not provided, defaults to yesterday's date.
+- --dataset-type (-t): Required. Specify the dataset type. Accepts 'roda' or 'guajira'.
+
+Examples:
+- CLI: python script.py --date 2023-12-01 --dataset-type roda
+- Docker: docker run --rm \
+		-v ~/.aws:/root/.aws \
+		-v $(shell pwd):/var/task \
+		-i --entrypoint python rodaapp:tribu_extraction \
+		lambda_extract_tribu_data.py --dataset-type roda --date 2023-12-01
+
+Output:
+- The script retrieves data from the Tribu API and writes it to a CSV file on AWS S3.
+
+Note:
+- The script requires access to AWS S3 for fetching credentials and storing output.
 """
 import requests
 import argparse
@@ -15,6 +51,17 @@ from utils import dicts_to_csv, validate_date, read_json_from_s3, \
 TRIBU_URL = "https://tribugps.com/controlador.php"
 
 def login(dataset_type):
+    """
+    Authenticate with the Tribu API using credentials based on the dataset type.
+
+    Fetches Tribu API credentials from an AWS S3 bucket and uses them to log in
+    to the Tribu API. The dataset type ('roda' or 'guajira') determines the specific credentials used,
+    corresponding to motorbike or bicycle data, respectively.
+
+    :param dataset_type: A string indicating the type of dataset ('roda' or 'guajira').
+    :return: A token string used for authenticated API requests.
+    :raises Exception: If the API response status is not 200.
+    """
     logger.info(f"Downloading tribu {dataset_type} credentials")
     tribu_credentials_path = os.path.join(RODAAPP_BUCKET_PREFIX, "credentials", f"tribu_{dataset_type}_credentials.json")
     tribu_credentials = read_json_from_s3(tribu_credentials_path)
@@ -38,6 +85,18 @@ def login(dataset_type):
 
 
 def get_tribu_data(token, date):
+    """
+    Fetch GPS data for Rappi drivers from the Tribu API for a given date.
+
+    Sends a request to the Tribu API to retrieve GPS data, using an authentication
+    token and the specified date. The data pertains to Rappi drivers using motorbikes ('roda') 
+    or bicycles ('guajira'), depending on the dataset type.
+
+    :param token: Authentication token obtained from the login function.
+    :param date: A datetime.date object representing the date for which data is to be retrieved.
+    :return: A list of dictionaries containing the GPS data.
+    :raises Exception: If the API response status is not 200.
+    """
     logger.info("Downloading routes from tribu API")
     one_day = timedelta(days=1)
     form_data = {
@@ -60,6 +119,18 @@ def get_tribu_data(token, date):
 
 
 def handler(event, context):
+    """
+    Handler function for processing Tribu API data retrieval tasks.
+
+    Intended for use as the entry point in AWS Lambda, but also supports local execution.
+    Retrieves and processes GPS data from the Tribu API for a given date, then stores
+    the data in a specified location. The 'dataset_type' in the event determines whether
+    the data is primarily motorbike ('roda') or bicycle ('guajira') related.
+
+    :param event: A dictionary containing 'dataset_type' and optionally 'processing_date'.
+                  If 'processing_date' is not provided, defaults to yesterday's date.
+    :param context: Context information provided by AWS Lambda (unused in this function).
+    """
     logger.setLevel(logging.INFO)
     logger.info(f"STARTING: Tribu data extraction task. Parameters: \n{event}")
     tribu_token = login(event["dataset_type"])
@@ -75,6 +146,13 @@ def handler(event, context):
 
 
 if __name__ == "__main__":
+    """
+    Main entry point for script execution.
+
+    Supports running in a Docker container, AWS Lambda, or directly via CLI.
+    Parses command-line arguments for dataset type and optional processing date.
+    Executes the handler function with the appropriate parameters.
+    """
     if 'AWS_LAMBDA_RUNTIME_API' in os.environ:
         # Running in AWS Lambda environment
         from awslambdaric import bootstrap

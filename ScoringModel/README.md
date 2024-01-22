1d79db15f049e51f72d2b8a893b979d6d830a753
Santi Gonzalez <gonzales06082002@gmail.com>
2024-01-22 11:44:30 -0500

    Deleted submodule config for Scoring

---
+++ ScoringModel/README.md
@@ -0,0 +1,15 @@
+# scoringmodel
+
+
+# Step by step for execution
+
+```sh
+python3 -m venv env
+source env/bin/activate
+pip install -r requirements.txt
+python3 main.py
+
+```
+**If you got the message "Archivo guardado: DF_contactos.xlsx" script was succesfully executed**
+
+Currently, the script gives you an Excel document with the dataframe hole, and in the last column for each record score was written
---
+++ ScoringModel/api_airtable.py
@@ -0,0 +1,23 @@
+from airtable import Airtable
+import pandas as pd
+import numpy as np
+
+def get_table_Airtable(table_name1, personal_access_token):
+
+    base_key = 'appL6Qzg4ivN62g2e'  # Tu Base ID
+    table_name = table_name1  # Nombre de tu tabla
+    api_key = personal_access_token  # Tu Personal Access Token
+
+    airtable = Airtable(base_key, table_name, api_key)
+
+    # Obtén todos los registros
+    records = airtable.get_all()
+
+    # Convierte los registros en una lista de diccionarios
+    records_dict = [record['fields'] for record in records]
+
+    # Conviértelo en un DataFrame
+    df = pd.DataFrame(records_dict)
+
+    # Imprime el DataFrame
+    return df

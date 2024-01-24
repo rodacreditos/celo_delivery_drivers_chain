141bfde6b4dc0c4c88bf371d9003f09cf5a9b53f
james-payan <james.payan@correounivalle.edu.co>
2024-01-24 18:46:18 -0500

    amended to deploy also the abi, to allow interaction with the contract

--- roda_celo_smart_contracts/migrations/2_deploy_contracts.js
+++ roda_celo_smart_contracts/migrations/2_deploy_contracts.js
@@ -5,6 +5,7 @@ module.exports = function (deployer) {
   deployer.deploy(RodaRoute).then(() => {
     let config = {
       RODA_ROUTE_CONTRACT_ADDR: RodaRoute.address,
+      RODA_ROUTE_CONTRACT_ABI: RodaRoute.abi
     };
     fs.writeFileSync('./credentials/roda_celo_contracts.json', JSON.stringify(config));
   });

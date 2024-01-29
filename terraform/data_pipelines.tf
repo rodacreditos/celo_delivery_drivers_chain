a510a074eafdb9d7babbbde562c11b6b9ba2a45d
Santi Gonzalez <gonzales06082002@gmail.com>
2024-01-29 14:32:56 -0500

    Cloudwatch removed (for now)

--- terraform/data_pipelines.tf
+++ terraform/data_pipelines.tf
@@ -1,14 +1,3 @@
-resource "aws_cloudwatch_event_rule" "scoring_model_schedule" {
-  name                = "scoring-model-daily"
-  schedule_expression = "cron(0 6 * * ? *)"  # Todos los días a las 6AM UTC 1AM Col
-}
-
-resource "aws_cloudwatch_event_target" "scoring_model_target" {
-  rule = aws_cloudwatch_event_rule.scoring_model_schedule.name
-  arn  = aws_lambda_function.scoring_model.arn
-  role_arn = aws_iam_role.cloudwatch_role.arn
-}
-
 resource "aws_cloudwatch_event_rule" "daily_trigger" {
   name                = "daily-trigger-at-6-am"
   schedule_expression = "cron(0 6 * * ? *)"
